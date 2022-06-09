b9044f3ff540dc72c5d7d4b23b0c8f7d386d2e3f
iThai <ithai@iThais-MacBook-Pro.local>
2022-06-09 16:21:15 -0700

    styled logo/outline animation

--- src/components/Home/Logo/main.logo.js
+++ src/components/Home/Logo/main.logo.js
@@ -42,7 +42,7 @@ const Logo = () => {
         className="solid-logo"
         ref={solidLogoRef}
         src={LogoCD}
-        alt="AV Automation"
+        alt="Code-Distllers Logo"
       />
 
       <svg
